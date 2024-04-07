9b2f689b0fc88f722481fc42532f03564cd524a9
sabrinawoo <sabrinawoo3895@gmail.com>
2024-04-06 18:22:44 -0700

    started trend

---
+++ src/components/Trending.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Trending = () => {
+
+    const trending = [
+        {
+          name: "ION LIGHTWEIGHT CHALK BAG",
+          description: "Light, durable chalk bag designed for easy access and lasting reliability.",
+          image: "./images/trending/ION_LIGHTWEIGHT_CHALK_BAG",
+          product: "https://arcteryx.com/ca/en/shop/ion-lightweight-chalk-bag"
+        },
+        {
+            name: "ION LIGHTWEIGHT CHALK BAG",
+            description: "Light, durable chalk bag designed for easy access and lasting reliability.",
+            image: "./images/trending/ION_LIGHTWEIGHT_CHALK_BAG",
+            product: "https://arcteryx.com/ca/en/shop/ion-lightweight-chalk-bag"
+        },
+      ]
+
+  return (
+    <div>
+      HI
+    </div>
+  )
+}
+
+export default Trending
